41c1eb7b143dc5dc9ef30f9d0227c39b275d43c0
SakuraBoy02 <kiselew.vana@yandex.ru>
2026-07-21 15:50:45 +0300

    StartMedium_Slider

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-const slider = document.querySelector('#slider');
-const sliderItems = Array.from(slider.children);
+// const slider = document.querySelector('#slider');
+// const sliderItems = Array.from(slider.children);
 
-sliderItems.forEach(function (slide, index){
-    console.log(slide);
+// sliderItems.forEach(function (slide, index){
+//     console.log(slide);
 
-    if (index !== 0) {
-        slide.classList.add('hidden');
-    }
+//     if (index !== 0) {
+//         slide.classList.add('hidden');
+//     }
 
-    slide.dataset.index = index;
+//     slide.dataset.index = index;
 
-    slide.addEventListener('click', function(){
-        slide.classList.add('hidden');
+//     slide.addEventListener('click', function(){
+//         slide.classList.add('hidden');
 
-        // let nextSlideIndex;
-        // if(index + 1 === sliderItems.length){
-        //     nextSlideIndex = 0;
-        // }else{
-        //     nextSlideIndex = index + 1;
-        // }
-        // тоже самое что if  A
-        //                    |
-        //                    |
-        //Тернарный оператор
-        let nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
+//         // let nextSlideIndex;
+//         // if(index + 1 === sliderItems.length){
+//         //     nextSlideIndex = 0;
+//         // }else{
+//         //     nextSlideIndex = index + 1;
+//         // }
+//         // тоже самое что if  A
+//         //                    |
+//         //                    |
+//         //Тернарный оператор
+//         let nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
 
-        // const nextSlideIndex = +slide.dataset.index + 1;
+//         // const nextSlideIndex = +slide.dataset.index + 1;
 
-        const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-        nextSlide.classList.remove('hidden');
-
-        
-    })
-});
+//         const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+//         nextSlide.classList.remove('hidden');
+//     })
+// });
