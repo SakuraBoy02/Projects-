3585a9789539affc9ce54abc560440f033092040
SakuraBoy02 <kiselew.vana@yandex.ru>
2026-07-22 15:23:54 +0300

    PreFinalArrow&Seporator

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-// const slider = document.querySelector('#slider');
-// const sliderItems = Array.from(slider.children);
 
-// sliderItems.forEach(function (slide, index){
-//     console.log(slide);
+// // Получаем элемент, который нужно скрывать
+// const element = document.getElementById('jump-vector');
 
-//     if (index !== 0) {
-//         slide.classList.add('hidden');
-//     }
+// // Функция для проверки, достигли ли мы конца
+// function atEndOfPage() {
+//   // Вычисляем, сколько пользователь уже прокрутил
+//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   console.log(scrollTop);
+  
+//   // Вычисляем общую высоту документа и высоту окна браузера
+//   const documentHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+//   console.log(documentHeight);
+//   const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+//   console.log(windowHeight);
 
-//     slide.dataset.index = index;
+//   // Проверяем, прокрутил ли пользователь до самого низа
+//   return scrollTop + windowHeight >= documentHeight;
+// }
 
-//     slide.addEventListener('click', function(){
-//         slide.classList.add('hidden');
-
-//         // let nextSlideIndex;
-//         // if(index + 1 === sliderItems.length){
-//         //     nextSlideIndex = 0;
-//         // }else{
-//         //     nextSlideIndex = index + 1;
-//         // }
-//         // тоже самое что if  A
-//         //                    |
-//         //                    |
-//         //Тернарный оператор
-//         let nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
-
-//         // const nextSlideIndex = +slide.dataset.index + 1;
-
-//         const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-//         nextSlide.classList.remove('hidden');
-//     })
+// // Привязываем функцию к событию прокрутки
+// window.addEventListener('scroll', function() {
+//   if (atEndOfPage()) {
+//     // Скрываем элемент (удаляем из DOM или меняем стиль)
+//     element.style.display = 'none'; // или element.remove()
+//   } else {
+//     // Показываем, если проскочили
+//     element.style.display = 'block';
+//     element.style.visibility = 'visible'; // если использовали visibility
+//   }
 // });
+
 var swiper = new Swiper('.mySwiper', {
     effect: 'coverflow',
     grabCursor: true,
@@ -44,3 +43,4 @@ var swiper = new Swiper('.mySwiper', {
         slideShadows:true,
     },
 });
+
