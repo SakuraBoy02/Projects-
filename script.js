05c048f2c0358e6778c75fd873a65827edb5bc2c
SakuraBoy02 <kiselew.vana@yandex.ru>
2026-07-22 10:00:04 +0300

    Swiper_SecondDevice

--- script.js
+++ script.js
@@ -31,3 +31,16 @@
 //         nextSlide.classList.remove('hidden');
 //     })
 // });
+var swiper = new Swiper('.mySwiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 5,
+        stretch: 0,
+        depth: 100,
+        modifier: 1.5,
+        slideShadows:true,
+    },
+});
