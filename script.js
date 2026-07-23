6a5eb39472bdac9e70a3be5b363a802944b5bba0
SakuraBoy02 <kiselew.vana@yandex.ru>
2026-07-23 10:13:35 +0300

    SecondDevice_Separator&Arrow

--- script.js
+++ script.js
@@ -1,46 +1,62 @@
+// const slider = document.querySelector('#slider');
+// const sliderItems = Array.from(slider.children);
 
-// // Получаем элемент, который нужно скрывать
-// const element = document.getElementById('jump-vector');
+// sliderItems.forEach(function (slide, index){
+//     console.log(slide);
 
-// // Функция для проверки, достигли ли мы конца
-// function atEndOfPage() {
-//   // Вычисляем, сколько пользователь уже прокрутил
-//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   console.log(scrollTop);
-  
-//   // Вычисляем общую высоту документа и высоту окна браузера
-//   const documentHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-//   console.log(documentHeight);
-//   const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-//   console.log(windowHeight);
+//     if (index !== 0) {
+//         slide.classList.add('hidden');
+//     }
 
-//   // Проверяем, прокрутил ли пользователь до самого низа
-//   return scrollTop + windowHeight >= documentHeight;
-// }
+//     slide.dataset.index = index;
 
-// // Привязываем функцию к событию прокрутки
-// window.addEventListener('scroll', function() {
-//   if (atEndOfPage()) {
-//     // Скрываем элемент (удаляем из DOM или меняем стиль)
-//     element.style.display = 'none'; // или element.remove()
-//   } else {
-//     // Показываем, если проскочили
-//     element.style.display = 'block';
-//     element.style.visibility = 'visible'; // если использовали visibility
-//   }
+//     slide.addEventListener('click', function(){
+//         slide.classList.add('hidden');
+
+//         // let nextSlideIndex;
+//         // if(index + 1 === sliderItems.length){
+//         //     nextSlideIndex = 0;
+//         // }else{
+//         //     nextSlideIndex = index + 1;
+//         // }
+//         // тоже самое что if  A
+//         //                    |
+//         //                    |
+//         //Тернарный оператор
+//         let nextSlideIndex = index + 1 === sliderItems.length ? 0 : index + 1;
+
+//         // const nextSlideIndex = +slide.dataset.index + 1;
+
+//         const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+//         nextSlide.classList.remove('hidden');
+//     })
 // });
 
-var swiper = new Swiper('.mySwiper', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-        rotate: 5,
-        stretch: 0,
-        depth: 100,
-        modifier: 1.5,
-        slideShadows:true,
-    },
+document.getElementById('btn_reapet').addEventListener('click', () => {
+    window.location.href = 'index.html';
 });
+var swiper = new Swiper('.mySwiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
 
+        slidesPerView: 'auto',
+        // loop: true,
+        // loopAdditionalSlides: 1,
+        
+        
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+          slideShadows: true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        // autoplay:{
+        //     delay:2000,
+        //     disableOnInteraction:false,
+        // }
+      });
